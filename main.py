3ea3663a30a89f8c102f831e848fdd9f6103f672
fakmalpradana <fakmalpradana@gmail.com>
2023-02-24 16:28:05 +0700

    add hex2rgb function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from utils.bantu import BacaData, Dataset
+from matplotlib import pyplot as plt
+
+import random
 
 img_dataset = BacaData('image').ambilRGB()
 msk_dataset = BacaData('mask').ambil()
@@ -8,3 +11,12 @@ X_data, Y_data = Dataset(img_dataset, msk_dataset, dem_dataset).splitXY()
 
 print(f'Ukuran data input (gambar dan dem) adalah {X_data.shape}')
 print(f'Ukuran data label atau data mask adalah {Y_data.shape}')
+
+# plot img
+image_number = random.randint(0, len(img_dataset))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(img_dataset[8])
+plt.subplot(122)
+plt.imshow(msk_dataset[8])
+plt.show()
