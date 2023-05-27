9724deefcc969a8c522f6c4635a5e52df46f71f8
fakmalpradana <fakmalpradana@gmail.com>
2023-05-27 21:25:46 +0700

    rev1 : add other indicator for eval

--- eval.py
+++ eval.py
@@ -5,8 +5,9 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from utils.hex2rgb import Label
 from sklearn import metrics as m
+from imblearn import metrics as mm
 
-x = cv2.imread('out/ugm_15.png')
+x = cv2.imread('out/ugm_35.png')
 y = cv2.imread('data/mask_ugm_65.png')
 
 x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
@@ -20,26 +21,32 @@ actual = Label(act).convert()
 
 cm = m.confusion_matrix(actual.flatten(), prediction.flatten(), labels=[1, 2, 3, 4, 5, 6])
 acc = m.accuracy_score(actual.flatten(), prediction.flatten())
+rec = mm.sensitivity_score(actual.flatten(), prediction.flatten(), average='macro')
+pre = m.precision_score(actual.flatten(), prediction.flatten(), average='weighted')
+spe = mm.specificity_score(actual.flatten(), prediction.flatten(), average='weighted')
 
 print(f'accuracy : {acc}')
+print(f'recall : {rec}')
+print(f'precision : {pre}')
+print(f'sensitivity : {spe}')
 
-# plot matrix konfusi
-label = ['Sawah', 'Sungai', 'Pepohonan', 'Jalan', 'Ground/RTH', 'Bangunan']
+# # plot matrix konfusi
+# label = ['Sawah', 'Sungai', 'Pepohonan', 'Jalan', 'Ground/RTH', 'Bangunan']
 
-plt.figure(figsize=(8,6), dpi=100)
+# plt.figure(figsize=(8,6), dpi=100)
 
-ax = sns.heatmap(
-    100*cm/np.sum(cm), 
-    annot=True, 
-    vmax=20, 
-    vmin=0, 
-    cmap='viridis',
-    xticklabels=label,
-    yticklabels=label,
-    )
+# ax = sns.heatmap(
+#     100*cm/np.sum(cm), 
+#     annot=True, 
+#     vmax=20, 
+#     vmin=0, 
+#     cmap='viridis',
+#     xticklabels=label,
+#     yticklabels=label,
+#     )
 
-ax.set_yticklabels(ax.get_yticklabels(), rotation=60)
-ax.set(xlabel='Hasil klasifikasi', ylabel='Data sebenarnya')
-ax.set_title('Matrix Konfusi')
+# ax.set_yticklabels(ax.get_yticklabels(), rotation=60)
+# ax.set(xlabel='Hasil klasifikasi', ylabel='Data sebenarnya')
+# ax.set_title('Matrix Konfusi')
 
-plt.show()
+# plt.show()
